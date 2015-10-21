cb4fe3e43137803273aeae2f7da40a70d2c13eb6
toyhammered <daniel@rassiner.com>
2015-10-20 17:38:55 -0700

    completed assignment 30 seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,36 @@
 include RandomData
 
-# Create Posts
-50.times do
+# Create a single unique Post
 
-	Post.create!(
-			title: RandomData.random_sentence,
-			body: RandomData.random_paragraph
-		)
-end
-posts = Post.all
-
-
-# Create Comments
-100.times do
-	Comment.create!(
-			post: posts.sample,
-			body: RandomData.random_paragraph
-		)
+unq_post = Post.find_or_create_by(title: "My First Unique Post!") do |post|
+	post.body = "This is now a unique body relating to the title"
 end
 
-puts "Seeding Finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+# Create a single unique Comment
+
+Comment.find_or_create_by(body: "Some awesome comment!") do |comment|
+	comment.post_id = unq_post.id
+end
+
+# # Create Posts
+# 50.times do
+
+# 	Post.create!(
+# 			title: RandomData.random_sentence,
+# 			body: RandomData.random_paragraph
+# 		)
+# end
+# posts = Post.all
+
+
+# # Create Comments
+# 100.times do
+# 	Comment.create!(
+# 			post: posts.sample,
+# 			body: RandomData.random_paragraph
+# 		)
+# end
+
+# puts "Seeding Finished"
+# puts "#{Post.count} posts created"
+# puts "#{Comment.count} comments created"
