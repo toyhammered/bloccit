3ce9eee5472819c1054bcc05b312539691ae97c6
toyhammered <daniel@rassiner.com>
2015-10-24 20:05:52 -0700

    completed assignment 33 crud part 3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
   	@posts = Post.all
+
   end
 
   def show
@@ -8,7 +9,7 @@ class PostsController < ApplicationController
   end
 
   def new
-  	@post = Post.new	
+  	@post = Post.new
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+
+  resources :questions	
   resources :posts
 
   get 'about' => 'welcome#about'
   root to: 'welcome#index'
 
- 
+
 end
