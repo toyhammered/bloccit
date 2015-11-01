ad439779f5b025d1545dcd3edef54907c85c8bb3
toyhammered <daniel@rassiner.com>
2015-11-01 13:46:59 -0800

    completed assignment 38 sign in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,6 +8,11 @@ module SessionsHelper
     session[:user_id] = nil
   end
 
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
+
   def current_user
     User.find_by(id: session[:user_id])
   end
