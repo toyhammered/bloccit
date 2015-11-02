6fb291c7013077d091c95dd6871de5ed3b875095
toyhammered <daniel@rassiner.com>
2015-11-02 13:24:58 -0800

    completed assignment 39 posts and users

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,14 @@ class Post < ActiveRecord::Base
 
 	default_scope{ order('created_at DESC') }
 
+	def self.ordered_by_title
+		order('title DESC')
+	end
+
+	def self.ordered_by_reverse_created_at
+		order('created_at ASC')
+	end
+
 	validates :title, length: { minimum: 5}, presence: true
 	validates :body, length: { minimum: 20}, presence: true
 	validates :topic, presence: true
