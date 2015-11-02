cfcffb18107d83073124448dec02d29d94a839ab
toyhammered <daniel@rassiner.com>
2015-11-02 11:04:04 -0800

    completed checkpoint 39 posts and users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,5 +21,5 @@ class SessionsController < ApplicationController
     flash[:notice] = "You've been signed out, come back soon!"
     redirect_to root_path
   end
-  
+
 end # Final End
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,14 +4,15 @@ include RandomData
 RSpec.describe Comment, type: :model do
 
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
   let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
 
 
   describe "attributes" do
-  	it "should respond to body" do
-  		expect(comment).to respond_to(:body)
-  	end
+    it "should respond to body" do
+      expect(comment).to respond_to(:body)
+    end
   end
 
 end # end of Comment spec helper
