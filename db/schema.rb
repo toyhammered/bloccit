57055c269d21eb9bf46e1cf4d83afa81810d1554
toyhammered <daniel@rassiner.com>
2015-11-10 13:36:00 -0800

    completed checkpoint 43 voting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107205735) do
+ActiveRecord::Schema.define(version: 20151110212400) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -56,10 +56,36 @@ ActiveRecord::Schema.define(version: 20151107205735) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "ratingables", force: :cascade do |t|
+    t.integer  "rating_id"
+    t.integer  "topic_id"
+    t.integer  "post_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "ratingables", ["post_id"], name: "index_ratingables_on_post_id"
+  add_index "ratingables", ["rateable_type", "rateable_id"], name: "index_ratingables_on_rateable_type_and_rateable_id"
+  add_index "ratingables", ["rating_id"], name: "index_ratingables_on_rating_id"
+  add_index "ratingables", ["topic_id"], name: "index_ratingables_on_topic_id"
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "severity",      default: 0
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  add_index "ratings", ["rateable_type", "rateable_id"], name: "index_ratings_on_rateable_type_and_rateable_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
@@ -77,5 +103,16 @@ ActiveRecord::Schema.define(version: 20151107205735) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
