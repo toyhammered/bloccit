2d8abeb32f1d5c22c38bbda3c76ee8540672b37e
toyhammered <daniel@rassiner.com>
2015-11-14 10:11:33 -0800

    completed checkpoint 45 public profiles

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,12 @@ Rails.application.routes.draw do
 
     resources :comments, only: [:create, :destroy]
     resources :favorites, only: [:create, :destroy]
-    
+
     post '/up-vote' => 'votes#up_vote', as: :up_vote
     post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resource :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,7 +3,7 @@ include RandomData
 
 RSpec.describe Topic, type: :model do
 
-  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:topic) { create(:topic) }
 
   it { should have_many(:posts) }
   it { should have_many(:labelings) }
