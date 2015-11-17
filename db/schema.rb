b2d19fca36d937139ff435d69f0b01a53636f34b
toyhammered <daniel@rassiner.com>
2015-11-17 13:52:53 -0800

    no method error for admin?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113213208) do
+ActiveRecord::Schema.define(version: 20151117004243) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 20151113213208) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -119,4 +119,10 @@ RSpec.describe User, type: :model do
     end
   end
 
+  describe "#generate_auth_token" do
+    it 'creates a token' do
+      expect(user.auth_token).to_not be_nil
+    end
+  end
+
 end # end of User Testing Model
