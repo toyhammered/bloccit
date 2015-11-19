3994618e708b53f1dc51fcd6d4c2983df0d86e36
toyhammered <daniel@rassiner.com>
2015-11-19 11:53:23 -0800

    halfway complete with adding posts/comments

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,15 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show]
-      resources :topics, only: [:index, :show]
+      resources :topics, only: [:index, :show] do
+        resources :posts, only: [:show]
+      end
+
+      resources :posts, only: [:index, :show] do
+        resources :comments, only: [:show]
+      end
+
+      resources :comments, only: [:index, :show]
     end
   end
 
