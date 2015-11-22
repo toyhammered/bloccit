396cc8ee3c7edb84c54a57300c2139a7dff4e00d
toyhammered <daniel@rassiner.com>
2015-11-21 21:18:51 -0800

    semi-completed assignment-49-sending-data

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -28,7 +28,7 @@ class Api::V1::TopicsController < Api::V1::BaseController
 
     if topic.valid?
       topic.save!
-      render json: topic.to_json, status: 201
+      render json: topic.to_json, status: 200
     else
       render json: {error: "Topic is invalid", status: 400}, status: 400
     end
@@ -43,12 +43,29 @@ class Api::V1::TopicsController < Api::V1::BaseController
      else
        render json: {error: "Topic destroy failed", status: 400}, status: 400
      end
+
+  end
+
+  def create_post
+    topic = Topic.find(params[:topic_id])
+    post = topic.posts.build(post_parmas)
+
+    if post.valid?
+      post.save!
+      render json: post.to_json, status: 200
+    else
+      render json: {error: "Post is invalid", status: 400}, status: 400
+    end
   end
 
   private
 
     def topic_params
       params.require(:topic).permit(:name, :description, :public)
+    end
+
+    def post_params
+      params.requrie(:post).permit(:title, :body)
     end
 
 end
